aef24e47e79a213fa708845df6370b5077fe91b3
Pedro <rm88246@fiap.com.br>
2024-08-18 10:44:20 -0300

    feat: persistência de metadados no banco de dados

--- src/main/java/com/example/estudoSpringBatch/BatchConfig.java
+++ src/main/java/com/example/estudoSpringBatch/BatchConfig.java
@@ -1,26 +1,56 @@
 package com.example.estudoSpringBatch;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
-	private final JobRepository jobRepository;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
 
-	public BatchConfig(JobRepository jobRepository) {
-		this.jobRepository = jobRepository;
-	}
-	
-	@Bean
-	Job arquivoDelimitadoJob(Step leituraArquivoDelimitadoStep) {
-		return new JobBuilder("arquivoDelimitadoJob", jobRepository)
-				.start(leituraArquivoDelimitadoStep)
-				.incrementer(new RunIdIncrementer())
-				.build();
-	}
+    public BatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    Job imprimeOlaJob(Step imprimeOlaStep) {
+        return new JobBuilder("imprimeOlaJob", jobRepository)
+                .start(imprimeOlaStep)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    Step imprimeOlaStep(ItemReader<String> itemReader, ItemWriter<String> itemWriter) {
+        return new StepBuilder("imprimeOlaStep", jobRepository)
+                .<String, String>chunk(1, transactionManager)
+                .reader(itemReader)
+                .writer(itemWriter)
+                .build();
+    }
+
+    @Bean
+    ItemReader<String> itemReader() {
+        List<String> data = Arrays.asList("Olá", "Mundo", "Spring Batch");
+        return new ListItemReader<>(data);
+    }
+
+    @Bean
+    ItemWriter<String> itemWriter() {
+        return items -> items.forEach(System.out::println);
+    }
 }
